87fb71789cb8bf3336ef200e1198cc39f27b7068
taly173 <talytabarrosdossantos@gmail.com>
2025-05-22 13:58:15 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,4 @@
+carro = input ('qual é o nome do carro:')
+valor = (input ('digite o valordo carro:'))
+consuto = input('digite o consumo do carro')
+print(f'Carro: {carro}')
---
+++ dotw.py
@@ -0,0 +1,17 @@
+dia = float(input('digite um número'))
+if dia == 1:
+    print('domingo')
+if dia == 2:
+    print('segunda-feira')  
+if dia == 3:
+      print('terça-feira')
+if dia == 4:
+   print('quarta-feira')  
+if dia == 5:
+    print('quinta-feira')
+if dia == 6:
+    print('sexta-feira')
+if dia == 7:
+    print('sábado')
+else:
+    print('número errado')
